003086ad4914f52a575d97ddadadfd10f4c2019a
QuocAnh189 <anhquoc18092003@gmail.com>
2025-02-21 23:05:13 +0700

    setup vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env": process.env,
+  },
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+      "@components": "/src/components",
+      "@constants": "/src/constants",
+      "@interfaces": "/src/interfaces",
+      "@layouts": "/src/layouts",
+      "@pages": "/src/pages",
+      "@redux": "/src/redux",
+      "@utils": "/src/utils",
+    },
+  },
+  optimizeDeps: {
+    exclude: ["js-big-decimal"],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern",
+        quietDeps: true,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+});
